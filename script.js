78a853c62ea1218ae72f4abab61eec7fae496477
Manish Kumar <krmanish2101@gmail.com>
2021-07-15 19:49:00 +0530

    adding behaviour

--- script.js
+++ script.js
@@ -6,6 +6,44 @@ var operator = null;
 for(var i=0; i<button.length; i++){
     button[i].addEventListener('click',function(){
         var value = this.getAttribute('data-value');
-        console.log(value);
+        if(value == '+'){
+            operator = '+';
+            num1 = parseFloat(display.innerHTML);
+            display.innerHTML = '';
+        }
+        else if(value == '-'){
+            operator = '-';
+            num1 = parseFloat(display.innerHTML);
+            display.innerHTML = '';
+        }
+        else if(value == '*'){
+            operator = '*';
+            num1 = parseFloat(display.innerHTML);
+            display.innerHTML = '';
+        }
+        else if(value == '/'){
+            operator = '/';
+            num1 = parseFloat(display.innerHTML);
+            display.innerHTML = '';
+        }
+        else if(value == '%'){
+            operator = '%';
+            num1 = parseFloat(display.innerHTML);
+            display.innerHTML = '';
+        }
+        else if(value == '='){
+            num2 = parseFloat(display.innerHTML);
+            display.innerHTML = eval(num1 + operator + num2);
+        }
+        else if(value == 'ac'){
+            display.innerHTML = '';
+        }
+        else if(value == 'sign'){
+            let num = parseFloat(display.innerHTML);
+            display.innerHTML = num > 0 ? num * -1 : num;
+        }
+        else{
+            display.innerHTML += value;
+        }
     });
 }
